8adbe46bb0f721054dc424ee96518bb676ac350e
Seth Lachman <lachmanseth17@gmail.com>
2019-04-25 16:19:58 -0400

    Added to basic styling.

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,12 +1,27 @@
 import React from 'react';
 
-const TodoForm = props => {
-  return (
-    <form className="todo-form" onSubmit={props.onSubmit}>
-      <input type="text" placeholder="add todo" value={props.todoValue} name="task" onChange={props.onChange}/>
-      <button>Add Todo</button><button type="button" onClick={() => props.onRemoveCompleted()}>Clear Completed</button>
-    </form>
-  );
+const TodoForm = (props) => {
+	return (
+		<div className="form-wrapper">
+			<form className="todo-form" onSubmit={props.onSubmit}>
+				<div className="form-field">
+					<input
+						type="text"
+						placeholder="add todo"
+						value={props.todoValue}
+						name="task"
+						onChange={props.onChange}
+					/>
+				</div>
+				<button className="btn">Add New Item</button>
+			</form>
+      <div className="clear-section">
+        <button className="clear-btn" onClick={() => props.onRemoveCompleted()}>
+            Clear Completed
+        </button>
+      </div>
+		</div>
+	);
 };
 
 export default TodoForm;
